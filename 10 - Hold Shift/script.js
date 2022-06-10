2655145f81ce761474c4494d131e89690ec773fe
Bulgak Vika <vika.bulgak@kruschecompany.com>
2022-06-10 13:11:42 +0300

    add project 11 - Custom Video Player

--- 10 - Hold Shift/script.js
+++ 10 - Hold Shift/script.js
@@ -1,49 +1,24 @@
-const canvas = document.querySelector('#draw');
-const ctx = canvas.getContext('2d');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-ctx.strokeStyle = '#BADA55';
-ctx.lineJoin = 'round';
-ctx.lineCap = 'round';
-ctx.lineWidth = 100;
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
 
-let isDrawing = false;
-let lastX = 0;
-let lastY = 0;
-let hue = 0;
-let direction = true;
+let lastChecked;
 
-function draw(e) {
-    if (!isDrawing) return;
-    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
-    ctx.beginPath();
-    ctx.moveTo(lastX, lastY);
-    ctx.lineTo(e.offsetX, e.offsetY);
-    ctx.stroke();
-    [lastX, lastY] = [e.offsetX, e.offsetY];
+function handleCheck(e) {
+    let inBetween = false;
+    if (e.shiftKey && this.checked) {
+        checkboxes.forEach(checkbox => {
+            console.log(checkbox);
+            if (checkbox === this || checkbox === lastChecked) {
+                inBetween = !inBetween;
+                console.log('Starting to check them in between!');
+            }
 
-    hue++;
-    if (hue >= 360) {
-        hue = 0;
-    }
-    if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
-        direction = !direction;
+            if (inBetween) {
+                checkbox.checked = true;
+            }
+        });
     }
 
-    if(direction) {
-        ctx.lineWidth++;
-    } else {
-        ctx.lineWidth--;
-    }
-
+    lastChecked = this;
 }
 
-canvas.addEventListener('mousedown', (e) => {
-    isDrawing = true;
-    [lastX, lastY] = [e.offsetX, e.offsetY];
-});
-
-
-canvas.addEventListener('mousemove', draw);
-canvas.addEventListener('mouseup', () => isDrawing = false);
-canvas.addEventListener('mouseout', () => isDrawing = false);
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
